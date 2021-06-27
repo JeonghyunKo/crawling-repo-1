d787e39aa47ab401468a2935b7c77e6f3b1a874f
edgar <forkwntkd96@gmail.com>
2021-06-28 03:21:35 +0900

    edgar upload htmlfile v1

--- web/se.py
+++ web/se.py
@@ -1,4 +1,6 @@
 from flask import *
+from db import login_mysql
+mydb, cursor = login_mysql.login()
 
 app = Flask(__name__)
 
@@ -7,7 +9,7 @@ def mainpage():
     from db import queries
     influencer = queries.main_influencer()
     brand = queries.main_brand()
-    ## influencer, brand = jsonify(influencer, brand)
+    ##influencer, brand = jsonify(influencer, brand)
 
     return render_template("index.html", influencer = influencer, brand = brand) 
     
@@ -17,4 +19,11 @@ def channel(chname):
     return 'Hello, %s'%(chname)    
 
 
+@app.route('/hed')
+def hello_pybo():
+    from db import queries
+    brand = queries.main_brand()
+    return render_template("test.html",brand = brand) 
+
 app.run(debug=True, port=5000)
+
