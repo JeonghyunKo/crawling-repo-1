1ba32f71ba4531238422ff8087ac2e56a8e2054e
edgar <forkwntkd96@gmail.com>
2021-07-01 19:46:18 +0900

    html files upload

---
+++ wepservice/app/se.py
@@ -0,0 +1,34 @@
+from flask import *
+from db import login_mysql
+mydb, cursor = login_mysql.login()
+
+app = Flask(__name__)
+
+@app.route("/main")
+def mainpage():
+    from db import queries
+    influencer = queries.main_influencer()
+    brand = queries.main_brand()
+    ##influencer, brand = jsonify(influencer, brand)
+
+    return render_template("main.html", influencer = influencer, brand = brand) 
+    
+
+@app.route('/channel/<chname>') 
+def channel(chname):
+    return 'Hello, %s'%(chname)    
+
+
+@app.route('/hed')
+def hello_pybo():
+    from db import queries
+    brand = queries.main_brand()
+    return render_template("test.html",brand = brand) 
+
+
+@app.route('/index')
+def indexpage():
+    return render_template('index.html')
+
+app.run(debug=True, port=5000)
+
